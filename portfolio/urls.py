efcd84669b1d54708a934957258e082d6068c18e
ntcapslmao <muhammad.ghazi51@ui.ac.id>
2026-08-31 16:15:52 +0700

    html and css implementation (basic)
    no further edits have been made. tutorial closely followed without any major changes (except image and bonus gold ship picture to test different pictures and live update testing)

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myportfolio project.
+URL configuration for portfolio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.1/topics/http/urls/
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from portfolio.views import landing_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page, name='landing_page'),
 ]
+
